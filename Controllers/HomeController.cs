3b8a509fbd5d7207d20e0f1cb1d1bd0684115f04
Neeraj Kumar <neeraj@MyLiveWell.onmicrosoft.com>
2021-09-28 11:33:30 +0100

    done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace WebApplication5.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
